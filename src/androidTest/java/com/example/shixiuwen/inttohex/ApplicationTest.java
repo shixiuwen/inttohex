482fea4a95d2ce2e8bcd2e41e6f139bc06b8048b
shixiuwen <shixiuwen1991@yeah.net>
2015-10-26 17:17:26 +0800

    Update ApplicationTest.java

--- src/androidTest/java/com/example/shixiuwen/inttohex/ApplicationTest.java
+++ src/androidTest/java/com/example/shixiuwen/inttohex/ApplicationTest.java
@@ -9,5 +9,6 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        //测试别人的第二处修改
     }
 }
