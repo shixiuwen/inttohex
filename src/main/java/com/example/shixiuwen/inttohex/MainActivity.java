263aa552368cd9d2700de3670b5108cb8f7f0f5d
ShiXiuwen <amos.shi@carbit.com.cn>
2015-10-26 17:43:08 +0800

    gaizgeng02

--- src/main/java/com/example/shixiuwen/inttohex/MainActivity.java
+++ src/main/java/com/example/shixiuwen/inttohex/MainActivity.java
@@ -30,9 +30,7 @@ public class MainActivity extends ActionBarActivity {
                 //测试其他人修改3
                 //测试自己修改3
                 //测试别人修改4(两处修改）)
-<<<<<<< HEAD
                 //别人也修改了
->>>>>>> 73edede2dff2d152a4bac1134e7866fdef848a14
             }
         });
     }
