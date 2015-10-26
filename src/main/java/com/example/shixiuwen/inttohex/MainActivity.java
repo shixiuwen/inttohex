8c2e3e2229e1265583ec505ed54d208e1a526fe8
shixiuwen <shixiuwen1991@yeah.net>
2015-10-26 17:16:37 +0800

    Update MainActivity.java

--- src/main/java/com/example/shixiuwen/inttohex/MainActivity.java
+++ src/main/java/com/example/shixiuwen/inttohex/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends ActionBarActivity {
                 //测试自己修改2
                 //测试其他人修改3
                 //测试自己修改3
+                //测试别人修改4(两处修改）)
             }
         });
     }
