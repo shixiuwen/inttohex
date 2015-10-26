e6e365a2545f221925fb215a5cc0bbb789b46c43
ShiXiuwen <amos.shi@carbit.com.cn>
2015-10-26 16:24:14 +0800

    修改了Mainactivity.class文件

--- src/main/java/com/example/shixiuwen/inttohex/MainActivity.java
+++ src/main/java/com/example/shixiuwen/inttohex/MainActivity.java
@@ -15,16 +15,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         final EditText etShi = (EditText) findViewById(R.id.etShi);
-        final EditText etShiliu = (EditText)findViewById(R.id.etShiliu);
-        Button btnZhuanhuan = (Button)findViewById(R.id.btnZhuanhuan);
+        final EditText etShiliu = (EditText) findViewById(R.id.etShiliu);
+        Button btnZhuanhuan = (Button) findViewById(R.id.btnZhuanhuan);
 
         btnZhuanhuan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String s = etShi.getText().toString();
-                String s1 = Integer.toHexString(Integer.parseInt(s));
                 etShiliu.setText(Integer.toHexString(Integer.parseInt(s)));
-                System.out.println(s1);
+
             }
         });
     }
